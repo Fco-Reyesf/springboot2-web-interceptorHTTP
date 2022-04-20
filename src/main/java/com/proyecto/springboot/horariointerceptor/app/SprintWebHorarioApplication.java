18f36f7c0760853acbd32864115c9c53f28e4956
Fco-Reyesf <freyes.info@gmail.com>
2022-04-19 21:27:10 -0400

    interceptores HTTP inicio

---
+++ src/main/java/com/proyecto/springboot/horariointerceptor/app/SprintWebHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.horariointerceptor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintWebHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintWebHorarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/springboot/horariointerceptor/app/SprintWebHorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.springboot.horariointerceptor.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintWebHorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
